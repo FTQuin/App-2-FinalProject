06b3b20c5b676855bcfde85cc09045a457d81a90
FTQuin <quin.j.adam@gmail.com>
2021-11-04 11:39:08 -0700

    Test commit

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //Quin: Test comment to ensure commits are going through
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
