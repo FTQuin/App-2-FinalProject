ef1cafc37209633e0b6b3945569288e9d172c561
Shea Holden <sheakholden@gmail.com>
2021-11-04 11:54:47 -0700

    test commit

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //Quin: Test comment to ensure commits are going through
+    //Shea test commit
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
