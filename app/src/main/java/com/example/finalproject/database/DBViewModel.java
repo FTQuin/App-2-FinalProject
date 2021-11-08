d3abc472dfc2cc2565e9851065fc36e97bf06f1b
FTQuin <quin.j.adam@gmail.com>
2021-11-07 21:37:48 -0800

    Updated ViewModel to include posts and comments

--- app/src/main/java/com/example/finalproject/database/DBViewModel.java
+++ app/src/main/java/com/example/finalproject/database/DBViewModel.java
@@ -12,14 +12,18 @@ public class DBViewModel extends AndroidViewModel {
     private DBRepository mRepository;
 
     private LiveData<List<Post>> mAllPosts;
+    private LiveData<List<Comment>> mAllComments;
 
     public DBViewModel(Application application) {
         super(application);
         mRepository = new DBRepository(application);
         mAllPosts = mRepository.getAllPosts();
+        mAllComments = mRepository.getAllComments();
     }
 
     LiveData<List<Post>> getAllPosts() { return mAllPosts; }
+    LiveData<List<Comment>> getAllComments() { return mAllComments; }
 
-    public void insert(Post post) { mRepository.insert(post); }
+    public void insertPost(Post post) { mRepository.insertPost(post); }
+    public void insertComment(Comment comment) { mRepository.insertComment(comment); }
 }
