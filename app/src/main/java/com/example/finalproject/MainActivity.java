5eb2d7feb6496dfae86fb7274fee964a8f015d89
Shea Holden <sheakholden@gmail.com>
2021-11-11 18:12:43 -0800

    added code to toggle new post fragment on button click

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.finalproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +18,11 @@ import android.view.Window;
 import com.example.finalproject.databinding.ActivityMainBinding;
 import com.example.finalproject.feed.FeedFragment;
 import com.example.finalproject.viewpost.ViewPostFragment;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -34,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
 
         feedFragment = new FeedFragment();
         newPostFragment = new NewPostFragment();
+
+
 
         //To remove top app title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -58,9 +69,19 @@ public class MainActivity extends AppCompatActivity {
         binding.newPostBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final Fragment fragmentInFrame = getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+
+                //If statement to allow clicking button to toggle fragment.
+                if (fragmentInFrame instanceof FeedFragment){
+                    Log.d("debug", "Feed fragment displayed. would show new post fragment");
+                    fragmentManager.beginTransaction().add(binding.fragmentContainerView.getId(),
+                            newPostFragment).addToBackStack(null).commit();
+                } else if (fragmentInFrame instanceof NewPostFragment){
+                    Log.d("debug", "new post fragment shown. would remove new post fragment.");
+                    fragmentManager.beginTransaction().remove(newPostFragment).commit();
+                }
+
                 // TODO: animate appearance of fragment. disable touch actions on feed feed fragment.
-                fragmentManager.beginTransaction().add(binding.fragmentContainerView.getId(),
-                        newPostFragment).commit();
             }
         });
 
