72568544cd712e58917b0aabcb20b3b4e03cc2b2
FTQuin <quin.j.adam@gmail.com>
2021-11-17 18:48:06 -0800

    deleted unused class PlaceholderContent.java

--- app/src/main/java/com/example/finalproject/viewpost/CommentAdapter.java
+++ app/src/main/java/com/example/finalproject/viewpost/CommentAdapter.java
@@ -8,14 +8,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.finalproject.database.Comment;
 import com.example.finalproject.databinding.FragmentCommentBinding;
-import com.example.finalproject.placeholder.PlaceholderContent.PlaceholderItem;
 
 import java.util.List;
 
-/**
- * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
- * TODO: Replace the implementation with code for your data type.
- */
 public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.ViewHolder> {
 
     private final List<Comment> commentList;
