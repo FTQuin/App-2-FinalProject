bbca1220813078c1e0b69594890de98f27b7ae8d
FTQuin <quin.j.adam@gmail.com>
2021-11-27 15:50:14 -0800

    Comments work now

--- app/src/main/java/com/example/anon/NewPostFragment.java
+++ app/src/main/java/com/example/anon/NewPostFragment.java
@@ -37,8 +37,6 @@ public class NewPostFragment extends Fragment {
 
     private FragmentNewPostBinding binding;
     private DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-    ;
-    private DatabaseReference mPostRef = mRootRef.child("posts");
 
     private String editTitle, editContent, date;
 
